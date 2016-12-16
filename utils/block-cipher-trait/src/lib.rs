e85cde09df2ee79701d072de6a3dcc1f59ce5c39
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2016-12-16 18:24:22 +0300

    Updated block-cipher-trait

--- utils/block-cipher-trait/src/lib.rs
+++ utils/block-cipher-trait/src/lib.rs
@@ -3,19 +3,45 @@
 extern crate generic_array;
 
 use generic_array::{GenericArray, ArrayLength};
+use generic_array::typenum::Unsigned;
 
 pub type Block<BlockSize> = GenericArray<u8, BlockSize>;
 
 pub trait BlockCipher {
     type BlockSize: ArrayLength<u8>;
 
-    fn new(key: &[u8]) -> Self;
-
     fn encrypt_block(&self, input: &Block<Self::BlockSize>,
                      output: &mut Block<Self::BlockSize>);
 
     fn decrypt_block(&self, input: &Block<Self::BlockSize>,
                      output: &mut Block<Self::BlockSize>);
+}
+
+pub trait BlockCipherFixKey:BlockCipher {
+    type KeySize: ArrayLength<u8>;
+
+    fn new(key: &GenericArray<u8, Self::KeySize>) -> Self;
+}
+
+pub trait BlockCipherVarKey:BlockCipher {
+    fn new(key: &[u8]) -> Self;
+}
+
+
+/// Temporary here before it's merged into generic-array
+#[inline]
+pub fn from_slice<T, N: ArrayLength<T>>(slice: &[T]) -> &GenericArray<T, N> {
+    assert_eq!(slice.len(), N::to_usize());
+    unsafe {
+        &*(slice.as_ptr() as *const GenericArray<T, N>)
+    }
+}
+
+impl<B: BlockCipherFixKey> BlockCipherVarKey for B {
+    fn new(key: &[u8]) -> Self {
+        assert_eq!(key.len(), B::KeySize::to_usize());
+        B::new(from_slice(key))
+    }
 }
 
 /*
