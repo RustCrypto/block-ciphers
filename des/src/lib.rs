4d54315b3f422977ba5d0abf7febb3a427c02a40
Gulshan Singh <gsingh2011@gmail.com>
2017-01-23 18:19:50 -0800

    des: added constants and implemented sbox function

--- des/src/lib.rs
+++ des/src/lib.rs
@@ -1,22 +1,42 @@
 extern crate block_cipher_trait;
 extern crate generic_array;
 
+mod consts;
+
 use block_cipher_trait::{Block, BlockCipher, BlockCipherFixKey};
 use generic_array::GenericArray;
 use generic_array::typenum::U8;
+
+use consts::SBOXES;
 
 #[derive(Copy, Clone)]
 struct Des {
 }
 
 impl Des {
+    fn apply_sboxes(
+        &self,
+        input: u64,
+    ) -> u64 {
+        let mut output: u64 = 0;
+        for i in 0..8 {
+            let sbox = SBOXES[i];
+            let val = (input >> (i * 6)) & 0x3F;
+            output |= (sbox[val as usize] as u64) << (i * 6);
+        }
+
+        output
+    }
+
+    fn apply_pbox(&self) {
+    }
 }
 
 impl BlockCipher for Des {
     type BlockSize = U8;
 
     fn encrypt_block(&self, input: &Block<U8>, output: &mut Block<U8>) {
-        unimplemented!()
+
     }
 
     fn decrypt_block(&self, input: &Block<U8>, output: &mut Block<U8>) {
