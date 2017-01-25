8fbe1145ff07d38c3a33121f37fcd5f140e0f6ae
Gulshan Singh <gsingh2011@gmail.com>
2017-01-24 16:31:01 -0800

    des: fixed FINAL_PBOX

--- des/src/consts.rs
+++ des/src/consts.rs
@@ -9,16 +9,15 @@ pub const INITIAL_PBOX: [u8; 64] = [
     62, 54, 46, 38, 30, 22, 14, 6,
 ];
 
-// TODO: This is off by one
 pub const FINAL_PBOX: [u8; 64] = [
-    40, 8, 48, 16, 56, 24, 64, 32,
     39, 7, 47, 15, 55, 23, 63, 31,
     38, 6, 46, 14, 54, 22, 62, 30,
     37, 5, 45, 13, 53, 21, 61, 29,
     36, 4, 44, 12, 52, 20, 60, 28,
     35, 3, 43, 11, 51, 19, 59, 27,
     34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41, 9, 49, 17, 57, 25,
+    33, 1, 41,  9, 49, 17, 57, 25,
+    32, 0, 40,  8, 48, 16, 56, 24,
 ];
 
 pub const EXPANSION_PBOX: [u8; 48] = [
@@ -37,6 +36,12 @@ pub const ROUND_PBOX: [u8; 32] = [
     18, 12, 29,  5, 21, 10,  3, 24,
 ];
 
+// These boxes are not the same ones that appear in the literature. Normally,
+// the first and the last bits of the six input bits are used to choose the row
+// and the middle four bits are used to choose the column. These sboxes are
+// rearranged so that the bottom four bits choose the column and the top two
+// bits choose the row. In other words, we can directly index the sbox array
+// with the 6 input bits to get the correct value.
 pub const SBOXES: [[u8; 64]; 8] = [
     [
         14,  0,  4, 15, 13,  7,  1,  4,  2, 14, 15,  2, 11, 13,  8,  1,
