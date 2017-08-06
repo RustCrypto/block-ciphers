60064a752cc4df0ef7c114f308c2ebbce632e64a
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2017-08-06 21:28:38 +0300

    Removed leftover comments in aesni

--- aesni/src/lib.rs
+++ aesni/src/lib.rs
@@ -42,12 +42,3 @@ mod u64x2;
 pub use aes128::Aes128;
 pub use aes192::Aes192;
 pub use aes256::Aes256;
-
-
-// Intel AES-NI whitepaper:
-//
-// https://software.intel.com/sites/default/files/article/165683/aes-wp-2012-09-22-v01.pdf
-//
-// For more details read this document from p.21:
-//
-// https://www.cosic.esat.kuleuven.be/ecrypt/AESday/slides/Use_of_the_AES_Instruction_Set.pdf
