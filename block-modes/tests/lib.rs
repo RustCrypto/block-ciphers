72b20cb79a98fc69d0cc6519472a580ea14f2f0d
newpavlov <newpavlov@gmail.com>
2018-10-04 15:58:19 +0300

    update block-modes dependencies. closes #33

--- block-modes/tests/lib.rs
+++ block-modes/tests/lib.rs
@@ -1,5 +1,5 @@
 //! Test vectors generated with OpenSSL
-extern crate aes_soft;
+extern crate aes;
 extern crate block_cipher_trait;
 extern crate block_modes;
 
@@ -7,7 +7,7 @@ use block_cipher_trait::generic_array::GenericArray;
 use block_modes::{BlockMode, BlockModeIv};
 use block_modes::block_padding::ZeroPadding;
 use block_modes::{Cbc, Cfb, Ctr128, Ecb, Ofb};
-use aes_soft::Aes128;
+use aes::Aes128;
 
 #[test]
 fn ecb_aes128() {
