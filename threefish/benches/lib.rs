07859abf0c4b57228dcf0cfaaa3d2e238a0d00fa
Kaz Wesley <kaz@lambdaverse.org>
2018-10-20 10:51:53 -0700

    threefish: fix benches for new API

--- threefish/benches/lib.rs
+++ threefish/benches/lib.rs
@@ -16,40 +16,34 @@ use generic_array::GenericArray;
 pub fn encrypt_1_256(bh: &mut Bencher) {
     let key = Default::default();
     let tweak = Default::default();
-    let state = threefish::Threefish256::new(&key, &tweak);
+    let state = threefish::Threefish256::with_tweak(&key, &tweak);
     let input = &[1u8; 32];
-    let mut output = GenericArray::default();
-
     bh.iter(|| {
-        state.encrypt_block(GenericArray::from_slice(input), &mut output);
+        state.encrypt_block(mut GenericArray::from_slice(input));
     });
     bh.bytes = 32u64;
 }
 
 #[bench]
 pub fn encrypt_2_512(bh: &mut Bencher) {
-    let key = [0u8; 64];
+    let key = Default::default();
     let tweak = Default::default();
-    let state = threefish::Threefish512::new(&key, &tweak);
+    let state = threefish::Threefish512::with_tweak(&key, &tweak);
     let input = &[1u8; 64];
-    let mut output = GenericArray::default();
-
     bh.iter(|| {
-        state.encrypt_block(GenericArray::from_slice(input), &mut output);
+        state.encrypt_block(mut GenericArray::from_slice(input));
     });
     bh.bytes = 64u64;
 }
 
 #[bench]
 pub fn encrypt_3_1024(bh: &mut Bencher) {
-    let key = [0u8; 128];
+    let key = Default::default();
     let tweak = Default::default();
-    let state = threefish::Threefish1024::new(&key, &tweak);
+    let state = threefish::Threefish1024::with_tweak(&key, &tweak);
     let input = &[1u8; 128];
-    let mut output = GenericArray::default();
-
     bh.iter(|| {
-        state.encrypt_block(GenericArray::from_slice(input), &mut output);
+        state.encrypt_block(mut GenericArray::from_slice(input));
     });
     bh.bytes = 128u64;
 }
