5fc3d4318997f8338647a66e05027272b37d7eb0
Luro02 <24826124+Luro02@users.noreply.github.com>
2019-08-26 09:04:45 +0200

    workaround for failing travis-ci

--- block-modes/src/errors.rs
+++ block-modes/src/errors.rs
@@ -17,13 +17,29 @@ impl fmt::Display for BlockModeError {
 }
 
 #[cfg(feature = "std")]
-impl error::Error for BlockModeError {}
+impl error::Error for BlockModeError {
+    // workaround for failing travis-ci tests with rust v1.22
+    // should be removed, once rust v1.22 is no longer supported:
+    // https://github.com/rust-lang/rust/blob/4c58535d09d1261d21569df0036b974811544256/
+    // src/libstd/error.rs#L69
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+}
 
 impl fmt::Display for InvalidKeyIvLength {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid key or IV length during block cipher mode initialization")
+        write!(f, "invalid key or IV length, during block cipher mode initialization")
     }
 }
 
 #[cfg(feature = "std")]
-impl error::Error for InvalidKeyIvLength {}
+impl error::Error for InvalidKeyIvLength {
+    // workaround for failing travis-ci tests with rust v1.22
+    // should be removed, once rust v1.22 is no longer supported:
+    // https://github.com/rust-lang/rust/blob/4c58535d09d1261d21569df0036b974811544256/
+    // src/libstd/error.rs#L69
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+}
