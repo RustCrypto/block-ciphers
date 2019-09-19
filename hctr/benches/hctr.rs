64a439bb3fd31dfd84b537216a86fac8824cfdff
James McGlashan <unreachable@darkfox.id.au>
2019-09-19 00:00:00 +0000

    HCTR documentation & generalized over BlockCipher + UniversalHash

--- hctr/benches/hctr.rs
+++ hctr/benches/hctr.rs
@@ -1,15 +1,22 @@
+#[cfg(feature = "polyval")]
 use core::convert::TryInto;
 
 use criterion::*;
 
-use ctr::{Ctr128, stream_cipher::{NewStreamCipher, SyncStreamCipher}};
-use aes::block_cipher_trait::BlockCipher;
-
-use aes::Aes128;
+#[cfg(feature = "aesni-bench")]
 use aesni::Aes128Ctr;
-use polyval::Polyval;
+#[cfg(feature = "aesni-bench")]
+use ctr::stream_cipher::NewStreamCipher;
 
-use hctr::Hctr;
+#[cfg(feature = "aes")]
+use aes::{block_cipher_trait::BlockCipher, Aes128};
+#[cfg(feature = "aes")]
+use ctr::{stream_cipher::SyncStreamCipher, Ctr128};
+#[cfg(feature = "polyval")]
+use polyval::{universal_hash::UniversalHash, Polyval};
+
+#[cfg(all(feature = "aes", feature = "polyval"))]
+use hctr::Aes128HctrPolyval;
 
 fn bench(c: &mut Criterion) {
     const KB: usize = 1024;
@@ -17,10 +24,28 @@ fn bench(c: &mut Criterion) {
     c.bench(
         "throughput",
         ParameterizedBenchmark::new(
+            "memcpy",
+            |b, &&size| {
+                let buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                b.iter(|| buf.clone());
+            },
+            &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 1280],
+        )
+        .throughput(|elems| Throughput::Bytes(**elems as u64)),
+    );
+
+    // FIXME: remove after aesni[ctr] performance isn't better than aes+ctr
+    #[cfg(feature = "aesni-bench")]
+    c.bench(
+        "throughput",
+        ParameterizedBenchmark::new(
             "ctr-ni",
             |b, &&size| {
-                let mut aes = Aes128Ctr::new(&Default::default(), &Default::default());
-                let mut buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                let mut aes =
+                    Aes128Ctr::new(&Default::default(), &Default::default());
+                let mut buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
                 b.iter(|| {
                     aes.apply_keystream(&mut buf);
                 });
@@ -30,6 +55,7 @@ fn bench(c: &mut Criterion) {
         .throughput(|elems| Throughput::Bytes(**elems as u64)),
     );
 
+    #[cfg(feature = "aes")]
     c.bench(
         "throughput",
         ParameterizedBenchmark::new(
@@ -37,7 +63,8 @@ fn bench(c: &mut Criterion) {
             |b, &&size| {
                 let aes = Aes128::new(&Default::default());
                 let mut cipher = Ctr128::from_cipher(aes, &Default::default());
-                let mut buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                let mut buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
                 b.iter(|| {
                     cipher.apply_keystream(&mut buf);
                 });
@@ -47,32 +74,19 @@ fn bench(c: &mut Criterion) {
         .throughput(|elems| Throughput::Bytes(**elems as u64)),
     );
 
+    #[cfg(feature = "polyval")]
     c.bench(
         "throughput",
         ParameterizedBenchmark::new(
-            "memcpy",
+            "polyval",
             |b, &&size| {
-                let buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
-                b.iter(|| {
-                    buf.clone()
-                });
-            },
-            &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 1280],
-        )
-        .throughput(|elems| Throughput::Bytes(**elems as u64)),
-    );
-
-    c.bench(
-        "throughput",
-        ParameterizedBenchmark::new(
-            "hash",
-            |b, &&size| {
-                let hasher = Polyval::new(Default::default());
-                let buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                let hasher = Polyval::new(&Default::default());
+                let buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
                 b.iter(|| {
                     let mut hasher = hasher.clone();
                     for chunk in buf.chunks(16) {
-                        hasher.input_block(chunk.try_into().unwrap());
+                        hasher.update_block(chunk.try_into().unwrap());
                     }
                     hasher.result()
                 });
@@ -82,13 +96,15 @@ fn bench(c: &mut Criterion) {
         .throughput(|elems| Throughput::Bytes(**elems as u64)),
     );
 
+    #[cfg(all(feature = "aes", feature = "polyval"))]
     c.bench(
         "throughput",
         ParameterizedBenchmark::new(
             "seal",
             |b, &&size| {
-                let hctr = Hctr::new([0u8; 32]);
-                let mut buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                let hctr = Aes128HctrPolyval::new(Default::default());
+                let mut buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
                 b.iter(|| hctr.seal_in_place(&mut buf, &[]));
             },
             &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 1280],
@@ -96,13 +112,15 @@ fn bench(c: &mut Criterion) {
         .throughput(|elems| Throughput::Bytes(**elems as u64)),
     );
 
+    #[cfg(all(feature = "aes", feature = "polyval"))]
     c.bench(
         "throughput",
         ParameterizedBenchmark::new(
             "open",
             |b, &&size| {
-                let hctr = Hctr::new([0u8; 32]);
-                let mut buf = core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
+                let hctr = Aes128HctrPolyval::new(Default::default());
+                let mut buf =
+                    core::iter::repeat(0u8).take(size).collect::<Vec<_>>();
                 b.iter(|| hctr.open_in_place(&mut buf, &[]));
             },
             &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB, 1280],
--- hctr/src/lib.rs
+++ hctr/src/lib.rs
@@ -1,19 +1,76 @@
-//! HCTR: http://delta.cs.cinvestav.mx/~debrup/hctr.pdf
+#![no_std]
+#![forbid(unsafe_code, missing_docs)]
 
-use core::convert::TryInto;
+//! [HCTR](http://delta.cs.cinvestav.mx/~debrup/hctr.pdf): A Variable-Input-Length Enciphering
+//! Mode.
+//!
+//! > HCTR turns an n-bit blockcipher into a tweakable blockcipher that supports arbitrary
+//! > variable input length which is no less than n bits. The tweak length of HCTR is fixed and
+//! > can be zero.
+//!
+//! This `Hctr` implementation is generic over the `BlockCipher` and `UniversalHash` functions,
+//! such that the `BlockSize` of the `BlockCipher` is equal to the `OutputSize` of the
+//! `UniversalHash`, and that the `UniversalHash` meets the additional constraints outlined in
+//! the HCTR paper. Additionally, the payload must be at least as large as the `BlockSize` of the
+//! provided `BlockCipher`.
+//!
+//! The tweak length may be variable if the AXU supports it, such as Polyval.
+//!
+//! ## Unstable
+//!
+//! There is currently no TweakableBlockCipher trait for HCTR to implement, thus its interface
+//! is unstable until one exists.
+//!
+//! ## Recommendations
+//!
+//! If the optional, and default, features "aes" and "polyval" are enabled; the recommended
+//! HCTR instantiation is available as `Aes128HctrPolyval`.
+//!
+//! If your target hardware supports accelerating either or both AES and Carryless Multiplication,
+//! make sure to enable the respective CPU target features for the best performance on your
+//! machine.
+//!
+//! `RUSTFLAGS="-Ctarget-cpu=native -Ctarget-feature=+aes,+sse2" cargo bench`
+//!
+//! ```rust
+//! # #[cfg(all(feature = "aes", feature = "polyval"))]
+//! # {
+//! use hctr::Aes128HctrPolyval as Hctr;
+//! # let secret_key = Default::default();
+//!
+//! let hctr = Hctr::new(secret_key);
+//! let mut buf = *b"Hello world! This message must be at least `BlockSize` large.";
+//! hctr.seal_in_place(&mut buf, b"a variable length byte string as the tweak.");
+//! hctr.open_in_place(&mut buf, b"a variable length byte string as the tweak.");
+//! assert_eq!(&buf[..], &b"Hello world! This message must be at least `BlockSize` large."[..]);
+//! # }
+//! ```
 
-use block_cipher_trait::{generic_array::GenericArray, BlockCipher};
+use block_cipher_trait::{
+    generic_array::{
+        typenum::{Sum, Unsigned, U16},
+        ArrayLength, GenericArray,
+    },
+    BlockCipher,
+};
 use ctr::{stream_cipher::SyncStreamCipher, Ctr128};
+use universal_hash::UniversalHash;
 
+#[cfg(feature = "aes")]
 use aes::Aes128;
+#[cfg(feature = "polyval")]
 use polyval::Polyval;
 
 #[derive(Clone)]
+/// See crate level documentation.
 pub struct Hctr<C, H> {
     cipher: C,
     hasher: H,
 }
 
+#[cfg(all(feature = "aes", feature = "polyval"))]
+/// HCTR instantiated using `Aes128` and `Polyval`. The recommended instantiation. Requires the
+/// default features "aes" and "polyval".
 pub type Aes128HctrPolyval = Hctr<Aes128, Polyval>;
 
 fn xor_in_place(a: &mut [u8], b: &[u8]) {
@@ -24,37 +81,62 @@ fn xor_in_place(a: &mut [u8], b: &[u8]) {
     }
 }
 
-fn hash(mut h: Polyval, a: &[u8], b: &[u8]) -> [u8; 16] {
-    h.input_padded(a);
-    h.input_padded(b);
+fn hash<H: UniversalHash>(
+    h: &mut H,
+    a: &[u8],
+    b: &[u8],
+) -> GenericArray<u8, H::OutputSize>
+{
+    h.update_padded(a);
+    h.update_padded(b);
 
-    *h.result().as_ref()
+    h.result_reset().into_bytes()
 }
 
-impl Hctr<Aes128, Polyval> {
-    pub fn new(key: [u8; 32]) -> Self {
-        let (a, b) = key.split_at(16);
+impl<C, H> Hctr<C, H>
+where
+    C: BlockCipher<BlockSize = U16> + Clone,
+    C::ParBlocks: ArrayLength<GenericArray<u8, C::BlockSize>>,
+    C::KeySize: core::ops::Add<H::KeySize>,
+    Sum<C::KeySize, H::KeySize>: ArrayLength<u8>,
+    Ctr128<C>: SyncStreamCipher,
+    H: UniversalHash<OutputSize = C::BlockSize>,
+{
+    /// Create a new HCTR instance with key size `C::KeySize + H::KeySize`.
+    pub fn new(key: GenericArray<u8, Sum<C::KeySize, H::KeySize>>) -> Self {
+        let (a, b) = key.split_at(C::KeySize::to_usize());
         Hctr {
-            cipher: Aes128::new(GenericArray::from_slice(a)),
-            hasher: Polyval::new(b.try_into().unwrap()),
+            cipher: C::new(GenericArray::from_slice(a)),
+            hasher: H::new(GenericArray::from_slice(b)),
         }
     }
 
-    pub fn seal_in_place(&self, buf: &mut [u8], aad: &[u8]) {
-        self.internal_prp(false, buf, aad);
+    /// Encrypt a message in-place with a tweak.
+    ///
+    /// Panics if the message length is not at least as large as the `BlockCipher`'s `BlockSize`.
+    pub fn seal_in_place(&self, buf: &mut [u8], tweak: &[u8]) {
+        self.internal_prp(false, buf, tweak);
     }
 
-    pub fn open_in_place(&self, buf: &mut [u8], aad: &[u8]) {
-        self.internal_prp(true, buf, aad);
+    /// Decrypt a message in-place with a tweak.
+    ///
+    /// Panics if the message length is not at least as large as the `BlockCipher`'s `BlockSize`.
+    pub fn open_in_place(&self, buf: &mut [u8], tweak: &[u8]) {
+        self.internal_prp(true, buf, tweak);
     }
 
-    fn internal_prp(&self, inverse: bool, buf: &mut [u8], aad: &[u8]) {
-        assert!(buf.len() >= 16, "message must be at least 16 bytes");
+    fn internal_prp(&self, inverse: bool, buf: &mut [u8], tweak: &[u8]) {
+        assert!(
+            buf.len() >= C::BlockSize::to_usize(),
+            "message must be at least as large as the BlockCipher::BlockSize."
+        );
 
-        let (l, r) = buf.split_at_mut(16);
+        let mut hasher = self.hasher.clone();
+
+        let (l, r) = buf.split_at_mut(C::BlockSize::to_usize());
 
         // phase 1: L ^= H(R, T)
-        xor_in_place(l, &hash(self.hasher.clone(), r, aad));
+        xor_in_place(l, &hash(&mut hasher, r, tweak));
 
         // phase 2a: L' = E(L)
         let mut internal_nonce = GenericArray::clone_from_slice(l);
@@ -68,16 +150,18 @@ impl Hctr<Aes128, Polyval> {
         // phase 2b: R = Ctr(L ^ L', R)
         xor_in_place(&mut *internal_nonce, l);
 
-        Ctr128::from_cipher(self.cipher.clone(), &internal_nonce).apply_keystream(r);
+        Ctr128::from_cipher(self.cipher.clone(), &internal_nonce)
+            .apply_keystream(r);
 
         // phase 3: L = L' ^ H(R, T)
-        xor_in_place(l, &hash(self.hasher.clone(), r, aad));
+        xor_in_place(l, &hash(&mut hasher, r, tweak));
     }
 }
 
 #[test]
+#[cfg(all(feature = "aes", feature = "polyval"))]
 fn weak_sanity_check() {
-    let hctr = Hctr::new(Default::default());
+    let hctr = Aes128HctrPolyval::new(Default::default());
     let a = include_bytes!("../LICENSE-MIT");
 
     let mut b = a.to_vec();
