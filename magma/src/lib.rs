4431d419c5bb5043c24acc6a747331058a546476
Артём Павлов <newpavlov@gmail.com>
2020-06-03 13:55:13 +0300

    re-enable no_std for magma and add dome docs

--- magma/src/lib.rs
+++ magma/src/lib.rs
@@ -1,6 +1,7 @@
-//! Pure Rust implementation of Magma (GOST 28147-89 and GOST R 34.12-2015) block cipher
-
-//#![no_std]
+//! Pure Rust implementation of the [Magma][1] (GOST 28147-89) block cipher.
+//!
+//! [1]: https://en.wikipedia.org/wiki/GOST_(block_cipher)
+#![no_std]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
 #![deny(unsafe_code)]
 #![warn(rust_2018_idioms)]
