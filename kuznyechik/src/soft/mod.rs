RustCrypto/block-ciphers

commit 736671fe85c23b29ff085b17820e47a609145b36

Update crates to cipher v0.4 (#284)

.github/workflows/aes.yml
@@ -23,7 +23,7 @@ jobs:
     strategy:
       matrix:
         rust:
-          - 1.49.0 # MSRV
+          - 1.56.0 # MSRV
           - stable
         target:
           - thumbv7em-none-eabi
@@ -36,13 +36,25 @@ jobs:
           target: ${{ matrix.target }}
           profile: minimal
           override: true
-      - run: cargo check --features compact,ctr,force-soft
-      - run: cargo build --release --target ${{ matrix.target }}
-      - run: cargo build --release --target ${{ matrix.target }} --features compact
-      - run: cargo build --release --target ${{ matrix.target }} --features ctr
-      - run: cargo build --release --target ${{ matrix.target }} --features force-soft
-      - run: cargo build --release --target ${{ matrix.target }} --features hazmat
-      - run: cargo build --release --target ${{ matrix.target }} --features compact,ctr,force-soft
+      - run: cargo check
+      - run: |
+        cargo build --target ${{ matrix.target }}
+        cargo build --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_force_soft"
+        run: |
+          cargo build --target ${{ matrix.target }}
+          cargo build --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_compact"
+        run: |
+          cargo build --target ${{ matrix.target }}
+          cargo build --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_force_soft --cfg aes_compact"
+        run: |
+          cargo build --target ${{ matrix.target }}
+          cargo build --target ${{ matrix.target }} --features hazmat
 
   # Tests for the AES-NI backend
   aesni:
@@ -56,7 +68,7 @@ jobs:
         include:
           # 32-bit Linux
           - target: i686-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
             deps: sudo apt update && sudo apt install gcc-multilib
           - target: i686-unknown-linux-gnu
             rust: stable
@@ -64,7 +76,7 @@ jobs:
 
           # 64-bit Linux
           - target: x86_64-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
           - target: x86_64-unknown-linux-gnu
             rust: stable
     steps:
@@ -76,12 +88,9 @@ jobs:
           profile: minimal
           override: true
       - run: ${{ matrix.deps }}
-      - run: cargo test --release --target ${{ matrix.target }}
-      - run: cargo test --release --target ${{ matrix.target }} --features compact
-      - run: cargo test --release --target ${{ matrix.target }} --features ctr
-      - run: cargo test --release --target ${{ matrix.target }} --features force-soft
-      - run: cargo test --release --target ${{ matrix.target }} --features hazmat
-      - run: cargo test --release --target ${{ matrix.target }} --all-features
+      - run: cargo test --target ${{ matrix.target }}
+      - run: cargo test --target ${{ matrix.target }} --features hazmat
+      - run: cargo test --target ${{ matrix.target }} --all-features
 
   # Tests for CPU feature autodetection with fallback to portable software implementation
   autodetect:
@@ -91,7 +100,7 @@ jobs:
         include:
           # 32-bit Linux
           - target: i686-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
             deps: sudo apt update && sudo apt install gcc-multilib
           - target: i686-unknown-linux-gnu
             rust: stable
@@ -99,7 +108,7 @@ jobs:
 
           # 64-bit Linux
           - target: x86_64-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
           - target: x86_64-unknown-linux-gnu
             rust: stable
     steps:
@@ -111,21 +120,21 @@ jobs:
           profile: minimal
           override: true
       - run: ${{ matrix.deps }}
-      - run: cargo test --release --target ${{ matrix.target }}
-      - run: cargo test --release --target ${{ matrix.target }} --features compact
-      - run: cargo test --release --target ${{ matrix.target }} --features ctr
-      - run: cargo test --release --target ${{ matrix.target }} --features hazmat
-      - run: cargo test --release --target ${{ matrix.target }} --all-features
+      - run: cargo test --target ${{ matrix.target }}
+      - run: cargo test --target ${{ matrix.target }} --features hazmat
+      - run: cargo test --target ${{ matrix.target }} --all-features
 
-  # Tests for the portable software backend (i.e. `force-soft`-only)
+  # Tests for the portable software backend (i.e. `aes_force_soft`-only)
   soft:
     runs-on: ubuntu-latest
+    env:
+      RUSTFLAGS: "-Dwarnings --cfg aes_force_soft"
     strategy:
       matrix:
         include:
           # 32-bit Linux
           - target: i686-unknown-linux-gnu
-            rust: 1.41.0 # MSRV (temporarily lower than with CPU feature auto-detection)
+            rust: 1.56.0 # MSRV
             deps: sudo apt update && sudo apt install gcc-multilib
           - target: i686-unknown-linux-gnu
             rust: stable
@@ -133,7 +142,7 @@ jobs:
 
           # 64-bit Linux
           - target: x86_64-unknown-linux-gnu
-            rust: 1.41.0 # MSRV (temporarily lower than with CPU feature auto-detection)
+            rust: 1.56.0 # MSRV
           - target: x86_64-unknown-linux-gnu
             rust: stable
     steps:
@@ -145,10 +154,9 @@ jobs:
           profile: minimal
           override: true
       - run: ${{ matrix.deps }}
-      - run: cargo test --release --target ${{ matrix.target }} --features force-soft
-      - run: cargo test --release --target ${{ matrix.target }} --features force-soft,compact
-      - run: cargo test --release --target ${{ matrix.target }} --features force-soft,ctr
-      - run: cargo test --release --target ${{ matrix.target }} --features force-soft,compact,ctr
+      - run: cargo test --target ${{ matrix.target }}
+      - run: cargo test --target ${{ matrix.target }} --all-features
+
 
   # Cross-compiled tests
   cross:
@@ -157,16 +165,20 @@ jobs:
         include:
           # ARM64
           - target: aarch64-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
           - target: aarch64-unknown-linux-gnu
             rust: stable
 
           # PPC32
           - target: powerpc-unknown-linux-gnu
-            rust: 1.49.0 # MSRV
+            rust: 1.56.0 # MSRV
           - target: powerpc-unknown-linux-gnu
             rust: stable
     runs-on: ubuntu-latest
+    # Cross mounts only current package, i.e. by default it ignores workspace's Cargo.toml
+    defaults:
+      run:
+        working-directory: .
     steps:
       - uses: actions/checkout@v1
       - run: ${{ matrix.deps }}
@@ -176,22 +188,45 @@ jobs:
           target: ${{ matrix.target }}
           profile: minimal
           override: true
-      - run: cargo install cross
-      - run: cross test --release --target ${{ matrix.target }}
-      - run: cross test --release --target ${{ matrix.target }} --features compact
-      - run: cross test --release --target ${{ matrix.target }} --features ctr
-      - run: cross test --release --target ${{ matrix.target }} --features force-soft
-      - run: cross test --release --target ${{ matrix.target }} --features hazmat
-      - run: cross test --release --target ${{ matrix.target }} --features compact,ctr,force-soft,hazmat
+      - name: Install pre-compiled cross
+        run: |
+          export URL=$(curl -s https://api.github.com/repos/cross-rs/cross/releases/latest | jq -r '.assets[] | select(.name | contains("x86_64-unknown-linux-gnu.tar.gz")) | .browser_download_url')
+          wget -O /tmp/binaries.tar.gz $URL
+          tar -C /tmp -xzf /tmp/binaries.tar.gz
+          mv /tmp/cross ~/.cargo/bin
+      - run: |
+        cross test --package aes --target ${{ matrix.target }}
+        cross test --package aes --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_force_soft"
+        run: |
+          cross test --package aes --target ${{ matrix.target }}
+          cross test --package aes --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_compact"
+        run: |
+          cross test --package aes --target ${{ matrix.target }}
+          cross test --package aes --target ${{ matrix.target }} --features hazmat
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg aes_force_soft --cfg aes_compact"
+        run: |
+          cross test --package aes --target ${{ matrix.target }}
+          cross test --package aes --target ${{ matrix.target }} --features hazmat
 
   # ARMv8 cross-compiled tests for AES intrinsics (nightly-only)
   armv8:
+    env:
+      RUSTFLAGS: "-Dwarnings --cfg aes_armv8"
     strategy:
       matrix:
         include:
           - target: aarch64-unknown-linux-gnu
             rust: nightly
     runs-on: ubuntu-latest
+    # Cross mounts only current package, i.e. by default it ignores workspace's Cargo.toml
+    defaults:
+      run:
+        working-directory: .
     steps:
       - uses: actions/checkout@v1
       - run: ${{ matrix.deps }}
@@ -201,22 +236,26 @@ jobs:
           target: ${{ matrix.target }}
           profile: minimal
           override: true
-      - run: cargo install cross
-      - run: cross test --release --target ${{ matrix.target }} --features armv8
-      - run: cross test --release --target ${{ matrix.target }} --features armv8,compact
-      - run: cross test --release --target ${{ matrix.target }} --features armv8,ctr
-      - run: cross test --release --target ${{ matrix.target }} --features armv8,force-soft
-      - run: cross test --release --target ${{ matrix.target }} --features armv8,hazmat
-      - run: cross test --release --target ${{ matrix.target }} --all-features
+      - name: Install pre-compiled cross
+        run: |
+          export URL=$(curl -s https://api.github.com/repos/cross-rs/cross/releases/latest | jq -r '.assets[] | select(.name | contains("x86_64-unknown-linux-gnu.tar.gz")) | .browser_download_url')
+          wget -O /tmp/binaries.tar.gz $URL
+          tar -C /tmp -xzf /tmp/binaries.tar.gz
+          mv /tmp/cross ~/.cargo/bin
+      - run: cross test --package aes --target ${{ matrix.target }}
+      - run: cross test --package aes --target ${{ matrix.target }} --features hazmat
+      - run: cross test --package aes --target ${{ matrix.target }} --all-features
 
   clippy:
+    env:
+      RUSTFLAGS: "-Dwarnings --cfg aes_compact"
     runs-on: ubuntu-latest
     steps:
       - uses: actions/checkout@v1
       - uses: actions-rs/toolchain@v1
         with:
-          toolchain: 1.49.0 # MSRV
+          toolchain: 1.56.0 # MSRV
           components: clippy
           override: true
           profile: minimal
-      - run: cargo clippy --features compact,ctr,hazmat -- -D warnings
+      - run: cargo clippy --features hazmat -- -D warnings

.github/workflows/kuznyechik.yml
@@ -22,7 +22,7 @@ jobs:
     strategy:
       matrix:
         rust:
-          - 1.41.0 # MSRV
+          - 1.56.0 # MSRV
           - stable
         target:
           - thumbv7em-none-eabi
@@ -35,14 +35,17 @@ jobs:
           toolchain: ${{ matrix.rust }}
           target: ${{ matrix.target }}
           override: true
-      - run: cargo build --release --target ${{ matrix.target }}
+      - run: cargo build --target ${{ matrix.target }}
+      - env:
+          RUSTFLAGS: "-Dwarnings --cfg kuznyechik_force_soft"
+        run: cargo build --target ${{ matrix.target }}
 
   test:
     runs-on: ubuntu-latest
     strategy:
       matrix:
         rust:
-          - 1.41.0 # MSRV
+          - 1.56.0 # MSRV
           - stable
     steps:
     - uses: actions/checkout@v1
@@ -51,7 +54,11 @@ jobs:
         toolchain: ${{ matrix.rust }}
         override: true
         profile: minimal
-    - run: cargo test
-    - run: cargo test --features no_unroll
-    - run: cargo test --features force-soft
-    - run: cargo test --features force-soft,no_unroll
+    - run: |
+      cargo test
+      cargo test --all-features
+    - env:
+        RUSTFLAGS: "-Dwarnings --cfg kuznyechik_force_soft"
+      run: |
+        cargo test
+        cargo test --all-features

kuznyechik/src/soft/mod.rs
@@ -1,161 +1,214 @@
-pub use cipher;
-
-use crate::consts::{P, P_INV};
+use crate::{BlockSize, Key, KeySize};
 use cipher::{
-    consts::{U1, U16, U32},
-    generic_array::GenericArray,
-    BlockCipher, BlockDecrypt, BlockEncrypt, NewBlockCipher,
+    AlgorithmName, BlockCipher, BlockClosure, BlockDecrypt, BlockEncrypt, BlockSizeUser, KeyInit,
+    KeySizeUser,
 };
+use core::fmt;
 
+#[cfg(feature = "zeroize")]
+use cipher::zeroize::{Zeroize, ZeroizeOnDrop};
+
+mod backends;
 mod consts;
 
-type Block = GenericArray<u8, U16>;
+use backends::{DecBackend, EncBackend, RoundKeys};
 
 /// Kuznyechik (GOST R 34.12-2015) block cipher
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Kuznyechik {
-    keys: [[u8; 16]; 10],
+    keys: RoundKeys,
 }
 
-#[inline(always)]
-fn x(a: &mut [u8; 16], b: &[u8; 16]) {
-    for i in 0..16 {
-        a[i] ^= b[i];
-    }
+impl BlockCipher for Kuznyechik {}
+
+impl KeySizeUser for Kuznyechik {
+    type KeySize = KeySize;
 }
 
-fn l_step(msg: &mut [u8; 16], i: usize) {
-    #[inline(always)]
-    fn get_idx(b: usize, i: usize) -> usize {
-        b.wrapping_sub(i) & 0x0F
-    }
-    #[inline(always)]
-    fn get_m(msg: &[u8; 16], b: usize, i: usize) -> usize {
-        msg[get_idx(b, i)] as usize
-    }
-
-    let mut x = msg[get_idx(15, i)];
-    x ^= consts::GF[3][get_m(msg, 14, i)];
-    x ^= consts::GF[1][get_m(msg, 13, i)];
-    x ^= consts::GF[2][get_m(msg, 12, i)];
-    x ^= consts::GF[0][get_m(msg, 11, i)];
-    x ^= consts::GF[5][get_m(msg, 10, i)];
-    x ^= consts::GF[4][get_m(msg, 9, i)];
-    x ^= msg[get_idx(8, i)];
-    x ^= consts::GF[6][get_m(msg, 7, i)];
-    x ^= msg[get_idx(6, i)];
-    x ^= consts::GF[4][get_m(msg, 5, i)];
-    x ^= consts::GF[5][get_m(msg, 4, i)];
-    x ^= consts::GF[0][get_m(msg, 3, i)];
-    x ^= consts::GF[2][get_m(msg, 2, i)];
-    x ^= consts::GF[1][get_m(msg, 1, i)];
-    x ^= consts::GF[3][get_m(msg, 0, i)];
-    msg[get_idx(15, i)] = x;
+impl BlockSizeUser for Kuznyechik {
+    type BlockSize = BlockSize;
 }
 
-#[inline(always)]
-fn lsx(msg: &mut [u8; 16], key: &[u8; 16]) {
-    x(msg, key);
-    // s
-    unroll16! {i, { msg[i] = P[msg[i] as usize]; }};
-    // l
-    unroll16! {i, { l_step(msg, i) }};
-}
-
-#[inline(always)]
-fn lsx_inv(msg: &mut [u8; 16], key: &[u8; 16]) {
-    x(msg, key);
-    // l_inv
-    unroll16! {i, { l_step(msg, 15 - i) }};
-    // s_inv
-    unroll16! {i, { msg[15 - i] = P_INV[msg[15 - i] as usize]; }};
-}
-
-fn get_c(n: usize) -> [u8; 16] {
-    let mut v = [0u8; 16];
-    v[15] = n as u8;
-    for i in 0..16 {
-        l_step(&mut v, i);
-    }
-    v
-}
-
-fn f(k1: &mut [u8; 16], k2: &mut [u8; 16], n: usize) {
-    for i in 0..4 {
-        let mut k1_cpy = *k1;
-        lsx(&mut k1_cpy, &get_c(8 * n + 2 * i + 1));
-        x(k2, &k1_cpy);
-
-        let mut k2_cpy = *k2;
-        lsx(&mut k2_cpy, &get_c(8 * n + 2 * i + 2));
-        x(k1, &k2_cpy);
-    }
-}
-
-impl Kuznyechik {
-    fn expand_key(&mut self, key: &GenericArray<u8, U32>) {
-        let mut k1 = [0u8; 16];
-        let mut k2 = [0u8; 16];
-
-        k1.copy_from_slice(&key[..16]);
-        k2.copy_from_slice(&key[16..]);
-
-        self.keys[0] = k1;
-        self.keys[1] = k2;
-
-        for i in 1..5 {
-            f(&mut k1, &mut k2, i - 1);
-            self.keys[2 * i] = k1;
-            self.keys[2 * i + 1] = k2;
+impl KeyInit for Kuznyechik {
+    fn new(key: &Key) -> Self {
+        Self {
+            keys: backends::expand(key),
         }
     }
+}
 
-    fn encrypt(&self, msg: &mut [u8; 16]) {
-        unroll9! {
-            i, { lsx(msg, &self.keys[i]) ; }
-        }
-        x(msg, &self.keys[9])
-    }
-
-    fn decrypt(&self, msg: &mut [u8; 16]) {
-        unroll9! {
-            i, { lsx_inv(msg, &self.keys[9 - i]) ; }
-        }
-        x(msg, &self.keys[0])
+impl From<KuznyechikEnc> for Kuznyechik {
+    #[inline]
+    fn from(enc: KuznyechikEnc) -> Kuznyechik {
+        Self { keys: enc.keys }
     }
 }
 
-impl NewBlockCipher for Kuznyechik {
-    type KeySize = U32;
-
-    fn new(key: &GenericArray<u8, U32>) -> Self {
-        let mut cipher = Self {
-            keys: Default::default(),
-        };
-        cipher.expand_key(key);
-        cipher
+impl From<&KuznyechikEnc> for Kuznyechik {
+    #[inline]
+    fn from(enc: &KuznyechikEnc) -> Kuznyechik {
+        Self {
+            keys: enc.keys.clone(),
+        }
     }
-}
-
-impl BlockCipher for Kuznyechik {
-    type BlockSize = U16;
-    type ParBlocks = U1;
 }
 
 impl BlockEncrypt for Kuznyechik {
-    #[inline]
-    fn encrypt_block(&self, block: &mut Block) {
-        #[allow(unsafe_code)]
-        let block: &mut [u8; 16] = unsafe { core::mem::transmute(block) };
-        self.encrypt(block);
+    fn encrypt_with_backend(&self, f: impl BlockClosure<BlockSize = BlockSize>) {
+        f.call(&mut EncBackend(&self.keys));
     }
 }
 
 impl BlockDecrypt for Kuznyechik {
-    #[inline]
-    fn decrypt_block(&self, block: &mut Block) {
-        #[allow(unsafe_code)]
-        let block: &mut [u8; 16] = unsafe { core::mem::transmute(block) };
-        self.decrypt(block);
+    fn decrypt_with_backend(&self, f: impl BlockClosure<BlockSize = BlockSize>) {
+        f.call(&mut DecBackend(&self.keys));
     }
 }
+
+impl fmt::Debug for Kuznyechik {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str("Kuznyechik { ... }")
+    }
+}
+
+impl AlgorithmName for Kuznyechik {
+    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("Kuznyechik")
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl Drop for Kuznyechik {
+    fn drop(&mut self) {
+        self.keys.zeroize();
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl ZeroizeOnDrop for Kuznyechik {}
+
+/// Kuznyechik (GOST R 34.12-2015) block cipher (encrypt-only)
+#[derive(Clone)]
+pub struct KuznyechikEnc {
+    keys: RoundKeys,
+}
+
+impl BlockCipher for KuznyechikEnc {}
+
+impl KeySizeUser for KuznyechikEnc {
+    type KeySize = KeySize;
+}
+
+impl BlockSizeUser for KuznyechikEnc {
+    type BlockSize = BlockSize;
+}
+
+impl KeyInit for KuznyechikEnc {
+    fn new(key: &Key) -> Self {
+        Self {
+            keys: backends::expand(key),
+        }
+    }
+}
+
+impl BlockEncrypt for KuznyechikEnc {
+    fn encrypt_with_backend(&self, f: impl BlockClosure<BlockSize = BlockSize>) {
+        f.call(&mut EncBackend(&self.keys));
+    }
+}
+
+impl fmt::Debug for KuznyechikEnc {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str("KuznyechikEnc { ... }")
+    }
+}
+
+impl AlgorithmName for KuznyechikEnc {
+    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("Kuznyechik")
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl Drop for KuznyechikEnc {
+    fn drop(&mut self) {
+        self.keys.zeroize();
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl ZeroizeOnDrop for KuznyechikEnc {}
+
+/// Kuznyechik (GOST R 34.12-2015) block cipher (decrypt-only)
+#[derive(Clone)]
+pub struct KuznyechikDec {
+    keys: RoundKeys,
+}
+
+impl BlockCipher for KuznyechikDec {}
+
+impl KeySizeUser for KuznyechikDec {
+    type KeySize = KeySize;
+}
+
+impl BlockSizeUser for KuznyechikDec {
+    type BlockSize = BlockSize;
+}
+
+impl KeyInit for KuznyechikDec {
+    fn new(key: &Key) -> Self {
+        Self {
+            keys: backends::expand(key),
+        }
+    }
+}
+
+impl From<KuznyechikEnc> for KuznyechikDec {
+    #[inline]
+    fn from(enc: KuznyechikEnc) -> KuznyechikDec {
+        Self { keys: enc.keys }
+    }
+}
+
+impl From<&KuznyechikEnc> for KuznyechikDec {
+    #[inline]
+    fn from(enc: &KuznyechikEnc) -> KuznyechikDec {
+        Self {
+            keys: enc.keys.clone(),
+        }
+    }
+}
+
+impl BlockDecrypt for KuznyechikDec {
+    fn decrypt_with_backend(&self, f: impl BlockClosure<BlockSize = BlockSize>) {
+        f.call(&mut DecBackend(&self.keys));
+    }
+}
+
+impl fmt::Debug for KuznyechikDec {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        f.write_str("KuznyechikDec { ... }")
+    }
+}
+
+impl AlgorithmName for KuznyechikDec {
+    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("Kuznyechik")
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl Drop for KuznyechikDec {
+    fn drop(&mut self) {
+        self.keys.zeroize();
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl ZeroizeOnDrop for KuznyechikDec {}

aes/benches/mod.rs
@@ -1,0 +1,62 @@
+#![feature(test)]
+extern crate test;
+
+use cipher::{block_decryptor_bench, block_encryptor_bench, KeyInit};
+
+block_encryptor_bench!(
+    Key: aes::Aes128,
+    aes128_encrypt_block,
+    aes128_encrypt_blocks,
+);
+block_decryptor_bench!(
+    Key: aes::Aes128,
+    aes128_decrypt_block,
+    aes128_decrypt_blocks,
+);
+block_encryptor_bench!(
+    Key: aes::Aes192,
+    aes192_encrypt_block,
+    aes192_encrypt_blocks,
+);
+block_decryptor_bench!(
+    Key: aes::Aes192,
+    aes192_decrypt_block,
+    aes192_decrypt_blocks,
+);
+block_encryptor_bench!(
+    Key: aes::Aes256,
+    aes256_encrypt_block,
+    aes256_encrypt_blocks,
+);
+block_decryptor_bench!(
+    Key: aes::Aes256,
+    aes256_decrypt_block,
+    aes256_decrypt_blocks,
+);
+
+#[bench]
+fn aes128_new(bh: &mut test::Bencher) {
+    bh.iter(|| {
+        let key = test::black_box(Default::default());
+        let cipher = aes::Aes128::new(&key);
+        test::black_box(&cipher);
+    });
+}
+
+#[bench]
+fn aes192_new(bh: &mut test::Bencher) {
+    bh.iter(|| {
+        let key = test::black_box(Default::default());
+        let cipher = aes::Aes192::new(&key);
+        test::black_box(&cipher);
+    });
+}
+
+#[bench]
+fn aes256_new(bh: &mut test::Bencher) {
+    bh.iter(|| {
+        let key = test::black_box(Default::default());
+        let cipher = aes::Aes256::new(&key);
+        test::black_box(&cipher);
+    });
+}

blowfish/benches/mod.rs
@@ -1,0 +1,16 @@
+#![feature(test)]
+extern crate test;
+
+use blowfish::Blowfish;
+use cipher::{block_decryptor_bench, block_encryptor_bench};
+
+block_encryptor_bench!(
+    Key: Blowfish,
+    blowfish_encrypt_block,
+    blowfish_encrypt_blocks,
+);
+block_decryptor_bench!(
+    Key: Blowfish,
+    blowfish_decrypt_block,
+    blowfish_decrypt_blocks,
+);

cast5/benches/mod.rs
@@ -1,0 +1,8 @@
+#![feature(test)]
+extern crate test;
+
+use cast5::Cast5;
+use cipher::{block_decryptor_bench, block_encryptor_bench};
+
+block_encryptor_bench!(Key: Cast5, cast5_encrypt_block, cast5_encrypt_blocks);
+block_decryptor_bench!(Key: Cast5, cast5_decrypt_block, cast5_decrypt_blocks);

commit 35bd0b40649c012c90d2b0a0ea6b9a324285b5f4

Fix `aes` and `kuznyechik` CI workflows
Looks like they were broken in #284

.github/workflows/aes.yml
@@ -3,6 +3,7 @@ name: aes
 on:
   pull_request:
     paths:
+      - ".github/workflows/aes.yml"
       - "aes/**"
       - "Cargo.*"
   push:
@@ -39,8 +40,8 @@ jobs:
           override: true
       - run: cargo check
       - run: |
-        cargo build --target ${{ matrix.target }}
-        cargo build --target ${{ matrix.target }} --features hazmat
+          cargo build --target ${{ matrix.target }}
+          cargo build --target ${{ matrix.target }} --features hazmat
       - env:
           RUSTFLAGS: "-Dwarnings --cfg aes_force_soft"
         run: |
@@ -205,8 +206,8 @@ jobs:
           tar -C /tmp -xzf /tmp/binaries.tar.gz
           mv /tmp/cross ~/.cargo/bin
       - run: |
-        cross test --package aes --target ${{ matrix.target }}
-        cross test --package aes --target ${{ matrix.target }} --features hazmat
+          cross test --package aes --target ${{ matrix.target }}
+          cross test --package aes --target ${{ matrix.target }} --features hazmat
       - env:
           RUSTFLAGS: "-Dwarnings --cfg aes_force_soft"
         run: |

.github/workflows/kuznyechik.yml
@@ -3,6 +3,7 @@ name: kuznyechik
 on:
   pull_request:
     paths:
+      - ".github/workflows/kuznyechik.yml"
       - "kuznyechik/**"
       - "Cargo.*"
   push:
@@ -62,8 +63,8 @@ jobs:
           override: true
           profile: minimal
       - run: |
-        cargo test
-        cargo test --all-features
+          cargo test
+          cargo test --all-features
       - env:
           RUSTFLAGS: "-Dwarnings --cfg kuznyechik_force_soft"
         run: |

kuznyechik/src/soft/mod.rs
@@ -81,7 +81,7 @@ impl AlgorithmName for Kuznyechik {
 #[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
 impl Drop for Kuznyechik {
     fn drop(&mut self) {
-        self.keys.zeroize();
+        self.keys.iter_mut().for_each(|key| key.zeroize());
     }
 }
 
@@ -135,7 +135,7 @@ impl AlgorithmName for KuznyechikEnc {
 #[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
 impl Drop for KuznyechikEnc {
     fn drop(&mut self) {
-        self.keys.zeroize();
+        self.keys.iter_mut().for_each(|key| key.zeroize());
     }
 }
 
@@ -205,7 +205,7 @@ impl AlgorithmName for KuznyechikDec {
 #[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
 impl Drop for KuznyechikDec {
     fn drop(&mut self) {
-        self.keys.zeroize();
+        self.keys.iter_mut().for_each(|key| key.zeroize());
     }
 }
 