586b5bf0c4b84132700dc5841524d43be266b445
Артём Павлов <newpavlov@gmail.com>
2022-09-15 16:38:28 +0300

    Code improvements

--- belt-block/src/lib.rs
+++ belt-block/src/lib.rs
@@ -1,4 +1,4 @@
-//! Pure Rust implementation of the [Belt-block][belt-block]
+//! Pure Rust implementation of the [BelT] block cipher.
 //!
 //! # ⚠️ Security Warning: Hazmat!
 //!
@@ -8,16 +8,22 @@
 //!
 //! USE AT YOUR OWN RISK!
 //!
-//! [belt-block]: https://ru.wikipedia.org/wiki/BelT
+//! [BelT]: https://ru.wikipedia.org/wiki/BelT
 
 #![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg"
+)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs, rust_2018_idioms)]
+#![forbid(unsafe_code)]
 
 use crate::consts::{H13, H21, H29, H5};
 pub use cipher;
 use cipher::consts::{U16, U32};
-use cipher::{AlgorithmName, BlockCipher, Key, KeyInit, KeySizeUser};
-use core::fmt;
-use core::ptr::{slice_from_raw_parts, slice_from_raw_parts_mut};
+use cipher::{inout::InOut, AlgorithmName, Block, BlockCipher, Key, KeyInit, KeySizeUser};
+use core::{fmt, mem::swap, num::Wrapping};
 
 mod consts;
 
@@ -28,11 +34,11 @@ macro_rules! g {
     ($($name:ident: ($a:expr, $b:expr, $c:expr, $d:expr)),+) => {
         $(
             #[inline]
-            fn $name(u: u32) -> u32 {
-                $a[((u >> 24) & 0xFF) as usize]
+            fn $name(Wrapping(u): Wrapping<u32>) -> Wrapping<u32> {
+                Wrapping($a[((u >> 24) & 0xFF) as usize]
                     ^ $b[((u >> 16) & 0xFF) as usize]
                     ^ $c[((u >> 8) & 0xFF) as usize]
-                    ^ $d[(u & 0xFF) as usize]
+                    ^ $d[(u & 0xFF) as usize])
             }
         )+
     }
@@ -44,128 +50,117 @@ g!(
     g21: (H13, H5, H29, H21)
 );
 
+/// BelT block cipher.
 #[derive(Clone)]
 pub struct BeltBlock {
-    pub(crate) key: [u32; 8],
+    key: [Wrapping<u32>; 8],
 }
 
-#[inline]
-#[allow(clippy::many_single_char_names)]
-/// Tact substitution, stands for 2.1-2.9 steps 6.1.3 algorithm (Block encryption) STB 34.101.31-2011
-fn tact_enc(i: u32, a: u32, b: u32, c: u32, d: u32, data: &mut [u32], key: &[u32]) {
-    // 2.1) b <- b xor G5(a+K[7i-6])
-    data[b as usize] ^= g5(data[a as usize].wrapping_add(key[(7 * i as usize - 6 - 1) % 8]));
-    // 2.2) c <- c xor G21(a+K[7i-5])
-    data[c as usize] ^= g21(data[d as usize].wrapping_add(key[(7 * i as usize - 5 - 1) % 8]));
-    // 2.3) a <- a - G13(a+K[7i-4])
-    data[a as usize] = data[a as usize].wrapping_sub(g13(
-        data[b as usize].wrapping_add(key[(7 * i as usize - 4 - 1) % 8])
-    ));
-    // 2.4) e <- G21(b+c+K[7i-3])+<i>_32
-    let e = g21(data[b as usize]
-        .wrapping_add(data[c as usize])
-        .wrapping_add(key[(7 * i as usize - 3 - 1) % 8]))
-        ^ i;
-    // 2.5) b <- b+e
-    data[b as usize] = data[b as usize].wrapping_add(e);
-    // 2.6) c <- c-e
-    data[c as usize] = data[c as usize].wrapping_sub(e);
-    // 2.7) d <- d xor G13(c+K[7i-2])
-    data[d as usize] = data[d as usize].wrapping_add(g13(
-        data[c as usize].wrapping_add(key[(7 * i as usize - 2 - 1) % 8])
-    ));
-    // 2.8) b <- b xor G21(a + K[7i-1])
-    data[b as usize] ^= g21(data[a as usize].wrapping_add(key[(7 * i as usize - 1 - 1) % 8]));
-    // 2.9) c <- c xor G5(d+K[7i])
-    data[c as usize] ^= g5(data[d as usize].wrapping_add(key[(7 * i as usize - 1) % 8]));
+#[inline(always)]
+fn get_u32(block: &[u8], i: usize) -> Wrapping<u32> {
+    Wrapping(u32::from_le_bytes(block[4 * i..][..4].try_into().unwrap()))
 }
 
-#[inline]
-#[allow(clippy::many_single_char_names)]
-/// Tact substitution, stands for 2.1-2.9 steps 6.1.4 algorithm (Block decryption) STB 34.101.31-2011
-fn tact_dec(i: u32, a: u32, b: u32, c: u32, d: u32, data: &mut [u32], key: &[u32]) {
-    // 2.1) b <- b xor G5(a+K[7i])
-    data[b as usize] ^= g5(data[a as usize].wrapping_add(key[(7 * i as usize - 1) % 8]));
-    // 2.2) c <- c xor G21(a+K[7i-1])
-    data[c as usize] ^= g21(data[d as usize].wrapping_add(key[(7 * i as usize - 1 - 1) % 8]));
-    // 2.3) a <- a - G13(a+K[7i-2])
-    data[a as usize] = data[a as usize].wrapping_sub(g13(
-        data[b as usize].wrapping_add(key[(7 * i as usize - 2 - 1) % 8])
-    ));
-    // 2.4) e <- G21(b+c+K[7i-3])+<i>_32
-    let e = g21(data[b as usize]
-        .wrapping_add(data[c as usize])
-        .wrapping_add(key[(7 * i as usize - 3 - 1) % 8]))
-        ^ i;
-    // 2.5) b <- b+e
-    data[b as usize] = data[b as usize].wrapping_add(e);
-    // 2.6) c <- c-e
-    data[c as usize] = data[c as usize].wrapping_sub(e);
-    // 2.7) d <- d xor G13(c+K[7i-4])
-    data[d as usize] = data[d as usize].wrapping_add(g13(
-        data[c as usize].wrapping_add(key[(7 * i as usize - 4 - 1) % 8])
-    ));
-    // 2.8) b <- b xor G21(a + K[7i-5])
-    data[b as usize] ^= g21(data[a as usize].wrapping_add(key[(7 * i as usize - 5 - 1) % 8]));
-    // 2.9) c <- c xor G5(d+K[7i-6])
-    data[c as usize] ^= g5(data[d as usize].wrapping_add(key[(7 * i as usize - 6 - 1) % 8]));
+#[inline(always)]
+fn set_u32(val: Wrapping<u32>, block: &mut [u8], i: usize) {
+    block[4 * i..][..4].copy_from_slice(&val.0.to_le_bytes());
+}
+
+#[inline(always)]
+fn idx(i: usize, delta: usize) -> usize {
+    (7 * i - delta - 1) % 8
 }
 
 impl BeltBlock {
+    /// Encryption as described in section 6.1.3
     #[inline]
-    pub(crate) fn encrypt(&self, data: &mut [u8; 16]) {
-        // Represent data as 4u32
-        let data_u32 = unsafe {
-            &mut *slice_from_raw_parts_mut(
-                data.as_mut_ptr() as *mut u32,
-                data.len() / core::mem::size_of::<u32>(),
-            )
-        };
+    fn encrypt(&self, mut block: InOut<'_, '_, Block<Self>>) {
+        let key = &self.key;
+        let block_in = block.get_in();
+        // Steps 1 and 4
+        let mut a = get_u32(block_in, 0);
+        let mut b = get_u32(block_in, 1);
+        let mut c = get_u32(block_in, 2);
+        let mut d = get_u32(block_in, 3);
 
-        // Execution 2.1-2.9 steps of block encryption algorithm
-        tact_enc(1, 0, 1, 2, 3, data_u32, &self.key);
-        tact_enc(2, 1, 3, 0, 2, data_u32, &self.key);
-        tact_enc(3, 3, 2, 1, 0, data_u32, &self.key);
-        tact_enc(4, 2, 0, 3, 1, data_u32, &self.key);
-        tact_enc(5, 0, 1, 2, 3, data_u32, &self.key);
-        tact_enc(6, 1, 3, 0, 2, data_u32, &self.key);
-        tact_enc(7, 3, 2, 1, 0, data_u32, &self.key);
-        tact_enc(8, 2, 0, 3, 1, data_u32, &self.key);
+        // Step 5
+        for i in 1..9 {
+            // 5.1) b <- b xor G5(a+K[7i-6])
+            b ^= g5(a + key[idx(i, 6)]);
+            // 5.2) c <- c xor G21(a+K[7i-5])
+            c ^= g21(d + key[idx(i, 5)]);
+            // 5.3) a <- a - G13(a+K[7i-4])
+            a -= g13(b + key[idx(i, 4)]);
+            // 5.4) e <- G21(b+c+K[7i-3])+<i>_32
+            let e = g21(b + c + key[idx(i, 3)]) ^ Wrapping(i as u32);
+            // 5.5) b <- b+e
+            b += e;
+            // 5.6) c <- c-e
+            c -= e;
+            // 5.7) d <- d xor G13(c+K[7i-2])
+            d += g13(c + key[idx(i, 2)]);
+            // 5.8) b <- b xor G21(a + K[7i-1])
+            b ^= g21(a + key[idx(i, 1)]);
+            // 5.9) c <- c xor G5(d+K[7i])
+            c ^= g5(d + key[idx(i, 0)]);
+            // 5.10-5.12)
+            swap(&mut a, &mut b);
+            swap(&mut c, &mut d);
+            swap(&mut b, &mut c);
+        }
 
-        // 10) a<->b
-        // 11) c<->d
-        // 12) b<->c
-        data_u32.swap(2, 3);
-        data_u32.swap(0, 1);
-        data_u32.swap(1, 2);
+        let block_out = block.get_out();
+        // Step 6
+        set_u32(b, block_out, 0);
+        set_u32(d, block_out, 1);
+        set_u32(a, block_out, 2);
+        set_u32(c, block_out, 3);
     }
 
+    /// Decryption as described in section 6.1.4
     #[inline]
-    pub(crate) fn decrypt(&self, data: &mut [u8; 16]) {
-        // Represent data as 4u32
-        let data_u32 = unsafe {
-            &mut *slice_from_raw_parts_mut(
-                data.as_mut_ptr() as *mut u32,
-                data.len() / core::mem::size_of::<u32>(),
-            )
-        };
+    fn decrypt(&self, mut block: InOut<'_, '_, Block<Self>>) {
+        let key = &self.key;
+        let block_in = block.get_in();
+        // Steps 1 and 4
+        let mut a = get_u32(block_in, 0);
+        let mut b = get_u32(block_in, 1);
+        let mut c = get_u32(block_in, 2);
+        let mut d = get_u32(block_in, 3);
 
-        // Execution 2.1-2.9 steps of block decryption algorithm
-        tact_dec(8, 0, 1, 2, 3, data_u32, &self.key);
-        tact_dec(7, 2, 0, 3, 1, data_u32, &self.key);
-        tact_dec(6, 3, 2, 1, 0, data_u32, &self.key);
-        tact_dec(5, 1, 3, 0, 2, data_u32, &self.key);
-        tact_dec(4, 0, 1, 2, 3, data_u32, &self.key);
-        tact_dec(3, 2, 0, 3, 1, data_u32, &self.key);
-        tact_dec(2, 3, 2, 1, 0, data_u32, &self.key);
-        tact_dec(1, 1, 3, 0, 2, data_u32, &self.key);
+        // Step 5
+        for i in (1..9).rev() {
+            // 5.1) b <- b xor G5(a+K[7i])
+            b ^= g5(a + key[idx(i, 0)]);
+            // 5.2) c <- c xor G21(a+K[7i-1])
+            c ^= g21(d + key[idx(i, 1)]);
+            // 5.3) a <- a - G13(a+K[7i-2])
+            a -= g13(b + key[idx(i, 2)]);
+            // 5.4) e <- G21(b+c+K[7i-3])+<i>_32
+            let e = g21(b + c + key[idx(i, 3)]) ^ Wrapping(i as u32);
+            // 5.5) b <- b+e
+            b += e;
+            // 5.6) c <- c-e
+            c -= e;
+            // 5.7) d <- d xor G13(c+K[7i-4])
+            d += g13(c + key[idx(i, 4)]);
+            // 5.8) b <- b xor G21(a + K[7i-5])
+            b ^= g21(a + key[idx(i, 5)]);
+            // 5.9) c <- c xor G5(d+K[7i-6])
+            c ^= g5(d + key[idx(i, 6)]);
 
-        // 10) a<->b
-        // 11) c<->d
-        // 12) a<->d
-        data_u32.swap(2, 3);
-        data_u32.swap(0, 3);
-        data_u32.swap(1, 3);
+            // 5.10-5.12)
+            swap(&mut a, &mut b);
+            swap(&mut c, &mut d);
+            swap(&mut a, &mut d);
+        }
+
+        let block_out = block.get_out();
+        // Step 6
+        set_u32(c, block_out, 0);
+        set_u32(a, block_out, 1);
+        set_u32(d, block_out, 2);
+        set_u32(b, block_out, 3);
     }
 }
 
@@ -177,22 +172,24 @@ impl KeySizeUser for BeltBlock {
 
 impl KeyInit for BeltBlock {
     fn new(key: &Key<Self>) -> Self {
-        let key: [u32; 8] = unsafe {
-            (&*slice_from_raw_parts(
-                key.as_ptr() as *const u32,
-                key.len() / core::mem::size_of::<u32>(),
-            ))
-                .try_into()
-                .unwrap()
-        };
-
-        Self { key }
+        Self {
+            key: [
+                get_u32(key, 0),
+                get_u32(key, 1),
+                get_u32(key, 2),
+                get_u32(key, 3),
+                get_u32(key, 4),
+                get_u32(key, 5),
+                get_u32(key, 6),
+                get_u32(key, 7),
+            ],
+        }
     }
 }
 
 impl AlgorithmName for BeltBlock {
     fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("belt-block")
+        f.write_str("BeltBlock")
     }
 }
 
@@ -211,13 +208,9 @@ impl ZeroizeOnDrop for BeltBlock {}
 cipher::impl_simple_block_encdec!(
     BeltBlock, U16, cipher, block,
     encrypt: {
-        let mut data = block.clone_in().into();
-        cipher.encrypt(&mut data);
-        block.get_out().copy_from_slice(&data);
+        cipher.encrypt(block);
     }
     decrypt: {
-        let mut data = block.clone_in().into();
-        cipher.decrypt(&mut data);
-        block.get_out().copy_from_slice(&data);
+        cipher.decrypt(block);
     }
 );
