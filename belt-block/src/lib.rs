f70dcd44cee1b3c5893f6adf460a03fb9948ab75
Артём Павлов <newpavlov@gmail.com>
2022-09-23 17:20:08 +0300

    belt-block: add belt_block_raw function

--- belt-block/src/lib.rs
+++ belt-block/src/lib.rs
@@ -53,70 +53,85 @@ g!(
 /// BelT block cipher.
 #[derive(Clone)]
 pub struct BeltBlock {
-    key: [Wrapping<u32>; 8],
+    key: [u32; 8],
 }
 
 #[inline(always)]
-fn get_u32(block: &[u8], i: usize) -> Wrapping<u32> {
-    Wrapping(u32::from_le_bytes(block[4 * i..][..4].try_into().unwrap()))
+fn get_u32(block: &[u8], i: usize) -> u32 {
+    u32::from_le_bytes(block[4 * i..][..4].try_into().unwrap())
 }
 
 #[inline(always)]
-fn set_u32(val: Wrapping<u32>, block: &mut [u8], i: usize) {
-    block[4 * i..][..4].copy_from_slice(&val.0.to_le_bytes());
+fn set_u32(block: &mut [u8], val: &[u32; 4], i: usize) {
+    block[4 * i..][..4].copy_from_slice(&val[i].to_le_bytes());
 }
 
 #[inline(always)]
-fn idx(i: usize, delta: usize) -> usize {
-    (7 * i - delta - 1) % 8
+fn key_idx(key: &[u32; 8], i: usize, delta: usize) -> Wrapping<u32> {
+    Wrapping(key[(7 * i - delta - 1) % 8])
+}
+
+/// Raw BelT block encryption function used for implementation of
+/// higher-level algorithms.
+#[inline(always)]
+pub fn belt_block_raw(x: [u32; 4], key: &[u32; 8]) -> [u32; 4] {
+    let mut a = Wrapping(x[0]);
+    let mut b = Wrapping(x[1]);
+    let mut c = Wrapping(x[2]);
+    let mut d = Wrapping(x[3]);
+
+    // Step 5
+    for i in 1..9 {
+        // 5.1) b ← b ⊕ G₅(a ⊞ k[7i-6])
+        b ^= g5(a + key_idx(key, i, 6));
+        // 5.2) c ← c ⊕ G₂₁(d ⊞ k[7i-5])
+        c ^= g21(d + key_idx(key, i, 5));
+        // 5.3) a ← a ⊟ G₁₃(b ⊞ k[7i-4])
+        a -= g13(b + key_idx(key, i, 4));
+        // 5.4) e ← G₂₁(b ⊞ c ⊞ k[7i-3]) ⊕ ⟨i⟩₃₂ ;
+        let e = g21(b + c + key_idx(key, i, 3)) ^ Wrapping(i as u32);
+        // 5.5) b ← b ⊞ e
+        b += e;
+        // 5.6) c ← c ⊟ e
+        c -= e;
+        // 5.7) d ← d ⊞ G₁₃(c ⊞ 𝑘[7i-2])
+        d += g13(c + key_idx(key, i, 2));
+        // 5.8) b ← b ⊕ G₂₁(a ⊞ 𝑘[(7i-1])
+        b ^= g21(a + key_idx(key, i, 1));
+        // 5.9) c ← c ⊕ G₅(d ⊞ 𝑘[7i])
+        c ^= g5(d + key_idx(key, i, 0));
+        // 5.10) a ↔ b
+        swap(&mut a, &mut b);
+        // 5.11) c ↔ d
+        swap(&mut c, &mut d);
+        // 5.12) b ↔ c
+        swap(&mut b, &mut c);
+    }
+
+    // Step 6
+    [b.0, d.0, a.0, c.0]
 }
 
 impl BeltBlock {
     /// Encryption as described in section 6.1.3
     #[inline]
     fn encrypt(&self, mut block: InOut<'_, '_, Block<Self>>) {
-        let key = &self.key;
         let block_in = block.get_in();
         // Steps 1 and 4
-        let mut a = get_u32(block_in, 0);
-        let mut b = get_u32(block_in, 1);
-        let mut c = get_u32(block_in, 2);
-        let mut d = get_u32(block_in, 3);
+        let x = [
+            get_u32(block_in, 0),
+            get_u32(block_in, 1),
+            get_u32(block_in, 2),
+            get_u32(block_in, 3),
+        ];
 
-        // Step 5
-        for i in 1..9 {
-            // 5.1) b ← b ⊕ G₅(a ⊞ k[7i-6])
-            b ^= g5(a + key[idx(i, 6)]);
-            // 5.2) c ← c ⊕ G₂₁(d ⊞ k[7i-5])
-            c ^= g21(d + key[idx(i, 5)]);
-            // 5.3) a ← a ⊟ G₁₃(b ⊞ k[7i-4])
-            a -= g13(b + key[idx(i, 4)]);
-            // 5.4) e ← G₂₁(b ⊞ c ⊞ k[7i-3]) ⊕ ⟨i⟩₃₂ ;
-            let e = g21(b + c + key[idx(i, 3)]) ^ Wrapping(i as u32);
-            // 5.5) b ← b ⊞ e
-            b += e;
-            // 5.6) c ← c ⊟ e
-            c -= e;
-            // 5.7) d ← d ⊞ G₁₃(c ⊞ 𝑘[7i-2])
-            d += g13(c + key[idx(i, 2)]);
-            // 5.8) b ← b ⊕ G₂₁(a ⊞ 𝑘[(7i-1])
-            b ^= g21(a + key[idx(i, 1)]);
-            // 5.9) c ← c ⊕ G₅(d ⊞ 𝑘[7i])
-            c ^= g5(d + key[idx(i, 0)]);
-            // 5.10) a ↔ b
-            swap(&mut a, &mut b);
-            // 5.11) c ↔ d
-            swap(&mut c, &mut d);
-            // 5.12) b ↔ c
-            swap(&mut b, &mut c);
-        }
+        let y = belt_block_raw(x, &self.key);
 
         let block_out = block.get_out();
         // 6) Y ← b ‖ d ‖ a ‖ c
-        set_u32(b, block_out, 0);
-        set_u32(d, block_out, 1);
-        set_u32(a, block_out, 2);
-        set_u32(c, block_out, 3);
+        for i in 0..4 {
+            set_u32(block_out, &y, i);
+        }
     }
 
     /// Decryption as described in section 6.1.4
@@ -125,31 +140,31 @@ impl BeltBlock {
         let key = &self.key;
         let block_in = block.get_in();
         // Steps 1 and 4
-        let mut a = get_u32(block_in, 0);
-        let mut b = get_u32(block_in, 1);
-        let mut c = get_u32(block_in, 2);
-        let mut d = get_u32(block_in, 3);
+        let mut a = Wrapping(get_u32(block_in, 0));
+        let mut b = Wrapping(get_u32(block_in, 1));
+        let mut c = Wrapping(get_u32(block_in, 2));
+        let mut d = Wrapping(get_u32(block_in, 3));
 
         // Step 5
         for i in (1..9).rev() {
             // 5.1) b ← b ⊕ G₅(a ⊞ 𝑘[7i])
-            b ^= g5(a + key[idx(i, 0)]);
+            b ^= g5(a + key_idx(key, i, 0));
             // 5.2) c ← c ⊕ G₂₁(d ⊞ 𝑘[7i-1])
-            c ^= g21(d + key[idx(i, 1)]);
+            c ^= g21(d + key_idx(key, i, 1));
             // 5.3) a ← a ⊟ G₁₃(b ⊞ 𝑘[7i-2])
-            a -= g13(b + key[idx(i, 2)]);
+            a -= g13(b + key_idx(key, i, 2));
             // 5.4) e ← G₂₁(b ⊞ c ⊞ 𝑘[7i-3]) ⊕ ⟨i⟩₃₂
-            let e = g21(b + c + key[idx(i, 3)]) ^ Wrapping(i as u32);
+            let e = g21(b + c + key_idx(key, i, 3)) ^ Wrapping(i as u32);
             // 5.5) b ← b ⊞ e
             b += e;
             // 5.6) c ← c ⊟ e
             c -= e;
             // 5.7) d ← d ⊞ G₁₃(c ⊞ 𝑘[7i-4])
-            d += g13(c + key[idx(i, 4)]);
+            d += g13(c + key_idx(key, i, 4));
             // 5.8) b ← b ⊕ G₂₁(a ⊞ 𝑘[7i-5])
-            b ^= g21(a + key[idx(i, 5)]);
+            b ^= g21(a + key_idx(key, i, 5));
             // 5.9) c ← c ⊕ G₅(d ⊞ 𝑘[7i-6])
-            c ^= g5(d + key[idx(i, 6)]);
+            c ^= g5(d + key_idx(key, i, 6));
             // 5.10) a ↔ b
             swap(&mut a, &mut b);
             // 5.11) c ↔ d
@@ -160,10 +175,10 @@ impl BeltBlock {
 
         let block_out = block.get_out();
         // 6) 𝑋 ← c ‖ a ‖ d ‖ b
-        set_u32(c, block_out, 0);
-        set_u32(a, block_out, 1);
-        set_u32(d, block_out, 2);
-        set_u32(b, block_out, 3);
+        let x = [c.0, a.0, d.0, b.0];
+        for i in 0..4 {
+            set_u32(block_out, &x, i);
+        }
     }
 }
 
@@ -200,9 +215,7 @@ impl AlgorithmName for BeltBlock {
 #[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
 impl Drop for BeltBlock {
     fn drop(&mut self) {
-        for val in self.key.iter_mut() {
-            val.0.zeroize();
-        }
+        self.key.zeroize();
     }
 }
 
