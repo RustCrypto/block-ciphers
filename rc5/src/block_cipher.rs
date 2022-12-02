6537488f8c2d58846fd4ee443a0f056328585f30
Antonio <adropulic@gmail.com>
2022-12-02 19:41:44 +0100

    add macro for cipher impl

--- rc5/src/block_cipher.rs
+++ rc5/src/block_cipher.rs
@@ -1,64 +1,70 @@
-use cipher::consts::{U16, U20, U8};
+use cipher::consts::{U16, U20};
 
-use crate::core::{ExpandedKeyTable, RC5};
+use crate::core::{BlockSize, ExpandedKeyTable, RC5};
 use cipher::{impl_simple_block_encdec, AlgorithmName, KeyInit};
 use cipher::{inout::InOut, Block, BlockCipher, KeySizeUser};
 
-pub struct RC5_32_20_16 {
-    key_table: ExpandedKeyTable<u32, U20>,
-}
-
-impl RC5<u32, U20, U16> for RC5_32_20_16 {}
-
-impl RC5_32_20_16 {
-    fn encrypt_block(&self, block: InOut<'_, '_, Block<Self>>) {
-        Self::encrypt(block, &self.key_table);
-    }
-
-    fn decrypt_block(&self, block: InOut<'_, '_, Block<Self>>) {
-        Self::decrypt(block, &self.key_table);
-    }
-}
-
-impl BlockCipher for RC5_32_20_16 {}
-
-impl KeySizeUser for RC5_32_20_16 {
-    type KeySize = U16;
-}
-
-impl KeyInit for RC5_32_20_16 {
-    fn new(key: &cipher::Key<Self>) -> Self {
-        Self {
-            key_table: Self::substitute_key(key),
+macro_rules! impl_rc5 {
+    (cipher: $cipher:ident, name: $name:expr, word: $word:ident, rounds: $rounds:ident, key_bytes: $key_bytes:ident) => {
+        pub struct $cipher {
+            key_table: ExpandedKeyTable<$word, $rounds>,
         }
-    }
+
+        impl RC5<$word, $rounds, $key_bytes> for $cipher {}
+
+        impl $cipher {
+            fn encrypt_block(&self, block: InOut<'_, '_, Block<Self>>) {
+                Self::encrypt(block, &self.key_table);
+            }
+
+            fn decrypt_block(&self, block: InOut<'_, '_, Block<Self>>) {
+                Self::decrypt(block, &self.key_table);
+            }
+        }
+
+        impl BlockCipher for $cipher {}
+
+        impl KeySizeUser for $cipher {
+            type KeySize = $key_bytes;
+        }
+
+        impl KeyInit for $cipher {
+            fn new(key: &cipher::Key<Self>) -> Self {
+                Self {
+                    key_table: Self::substitute_key(key),
+                }
+            }
+        }
+
+        impl AlgorithmName for $cipher {
+            fn write_alg_name(f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.write_str(stringify!($name))
+            }
+        }
+
+        impl_simple_block_encdec!(
+            $cipher, BlockSize<$word>, cipher, block,
+            encrypt: {
+                cipher.encrypt_block(block);
+            }
+            decrypt: {
+                cipher.decrypt_block(block);
+            }
+        );
+
+        #[cfg(feature = "zeroize")]
+        use cipher::zeroize::Zeroize;
+
+        #[cfg(feature = "zeroize")]
+        impl cipher::zeroize::ZeroizeOnDrop for $cipher {}
+
+        #[cfg(feature = "zeroize")]
+        impl Drop for $cipher {
+            fn drop(&mut self) {
+                self.key_table.zeroize()
+            }
+        }
+    };
 }
 
-impl AlgorithmName for RC5_32_20_16 {
-    fn write_alg_name(f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.write_str("RC5-32/20/16")
-    }
-}
-
-impl_simple_block_encdec!(
-    RC5_32_20_16, U8, cipher, block,
-    encrypt: {
-        cipher.encrypt_block(block);
-    }
-    decrypt: {
-        cipher.decrypt_block(block);
-    }
-);
-
-#[cfg(feature = "zeroize")]
-use cipher::zeroize::Zeroize;
-
-#[cfg(feature = "zeroize")]
-impl cipher::zeroize::ZeroizeOnDrop for RC5_32_20_16 {}
-
-#[cfg(feature = "zeroize")]
-impl Drop for RC5_32_20_16 {
-    fn drop(&mut self) {
-        self.key_table.zeroize()
-    }
-}
+impl_rc5!(cipher: RC5_32_20_16, name: "RC5 - 32/16/16", word: u32, rounds: U20, key_bytes: U16);
