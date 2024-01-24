9db88e9ae7ead87ef2217d76870fe86d233370bd
Artyom Pavlov <newpavlov@gmail.com>
2024-01-24 14:44:52 +0300

    fix tests

--- belt-block/tests/mod.rs
+++ belt-block/tests/mod.rs
@@ -124,3 +124,12 @@ fn belt_wblock() {
         assert_eq!(t, x[..i]);
     }
 }
+
+fn to_u32<const N: usize>(src: &[u8]) -> [u32; N] {
+    assert_eq!(src.len(), 4 * N);
+    let mut res = [0u32; N];
+    res.iter_mut()
+        .zip(src.chunks_exact(4))
+        .for_each(|(dst, src)| *dst = u32::from_le_bytes(src.try_into().unwrap()));
+    res
+}
