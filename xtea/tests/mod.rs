12a5d6408f9e718dce3316e445bf1c63e24a14ec
Tony Arcieri <bascule@gmail.com>
2024-07-28 18:58:36 -0600

    gift+xtea: bump dependencies (#433)
    - Updates `cipher` to v0.5.0-pre.6
    - Sets both crates' versions to 0.0.0
    - Renames gift to `gift-cipher` which is presently unregistered

--- xtea/tests/mod.rs
+++ xtea/tests/mod.rs
@@ -9,7 +9,7 @@ fn xtea() {
     let ciphertext = [0xea, 0x0c, 0x3d, 0x7c, 0x1c, 0x22, 0x55, 0x7f];
     let cipher = Xtea::new_from_slice(key).unwrap();
 
-    let mut block = Array::clone_from_slice(plaintext);
+    let mut block = Array(*plaintext);
     cipher.encrypt_block(&mut block);
     assert_eq!(ciphertext, block.as_slice());
 
