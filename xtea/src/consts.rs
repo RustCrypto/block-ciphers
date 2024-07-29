0be7856962da133dc40bdfdf76c9599437b7411b
Valaphee The Meerkat <32491319+valaphee@users.noreply.github.com>
2024-07-29 02:05:07 +0200

    XTEA implementation (#422)
    This PR implements XTEA as described by various sources, including
    https://en.wikipedia.org/wiki/XTEA,
    
    XTEA is a historical cipher, which is no longer commonly used today, but
    some legacy software still uses it to this day.
    
    I tested that the cipher methods get properly unrolled, which boosts a
    3x speed increase, and is one of the faster ciphers in this collection.
    (Hopefully there will be an unroll pragma sometime in the future)
    
    Partially solves https://github.com/RustCrypto/block-ciphers/issues/1
    
    I decided against pre-computing keys as the computation is trivial, and
    doesn't have a noticeable impact on speed, probably because of memory
    accesses and that 256 byte don't fit easily in a cache line anyway.

---
+++ xtea/src/consts.rs
@@ -0,0 +1,2 @@
+pub const DELTA: u32 = 0x9e3779b9;
+pub const ROUNDS: usize = 32;
---
+++ xtea/src/lib.rs
@@ -0,0 +1,156 @@
+//! Pure Rust implementation of the [Extended Tiny Encryption Algorithm][XTEA].
+//!
+//! # ⚠️ Security Warning: Hazmat!
+//!
+//! This crate implements only the low-level block cipher function, and is intended
+//! for use for implementing higher-level constructions *only*. It is NOT
+//! intended for direct use in applications.
+//!
+//! USE AT YOUR OWN RISK!
+//!
+//! [XTEA]: https://en.wikipedia.org/wiki/XTEA
+
+#![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg"
+)]
+#![deny(unsafe_code)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs, rust_2018_idioms)]
+
+pub use cipher;
+
+use cipher::{
+    consts::{U16, U8},
+    AlgorithmName, BlockCipher, InvalidLength, Key, KeyInit, KeySizeUser,
+};
+use core::fmt;
+
+#[cfg(feature = "zeroize")]
+use cipher::zeroize::{Zeroize, ZeroizeOnDrop};
+
+mod consts;
+use consts::{DELTA, ROUNDS};
+
+/// XTEA block cipher.
+pub struct Xtea {
+    k: [u32; 4],
+}
+
+impl BlockCipher for Xtea {}
+
+impl KeySizeUser for Xtea {
+    type KeySize = U16;
+}
+
+impl KeyInit for Xtea {
+    fn new(key: &Key<Self>) -> Self {
+        Self::new_from_slice(key).unwrap()
+    }
+
+    fn new_from_slice(key: &[u8]) -> Result<Self, InvalidLength> {
+        if key.len() != 16 {
+            return Err(InvalidLength);
+        }
+        let key = [
+            u32::from_le_bytes(key[0..4].try_into().unwrap()),
+            u32::from_le_bytes(key[4..8].try_into().unwrap()),
+            u32::from_le_bytes(key[8..12].try_into().unwrap()),
+            u32::from_le_bytes(key[12..16].try_into().unwrap()),
+        ];
+        Ok(Xtea { k: key })
+    }
+}
+
+impl fmt::Debug for Xtea {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("XTEA { ... }")
+    }
+}
+
+impl AlgorithmName for Xtea {
+    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("XTEA")
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl Drop for Xtea {
+    fn drop(&mut self) {
+        self.k.zeroize();
+    }
+}
+
+#[cfg(feature = "zeroize")]
+#[cfg_attr(docsrs, doc(cfg(feature = "zeroize")))]
+impl ZeroizeOnDrop for Xtea {}
+
+cipher::impl_simple_block_encdec!(
+    Xtea, U8, cipher, block,
+    encrypt: {
+        let v = block.get_in();
+        let mut v0 = u32::from_le_bytes(v[0..4].try_into().unwrap());
+        let mut v1 = u32::from_le_bytes(v[4..8].try_into().unwrap());
+        let mut sum = 0u32;
+
+        // Use 4 loops as otherwise unrolling will not be performed by default
+        for _ in 0..8 {
+            v0 = v0.wrapping_add((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+            sum = sum.wrapping_add(DELTA);
+            v1 = v1.wrapping_add((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v0 = v0.wrapping_add((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+            sum = sum.wrapping_add(DELTA);
+            v1 = v1.wrapping_add((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v0 = v0.wrapping_add((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+            sum = sum.wrapping_add(DELTA);
+            v1 = v1.wrapping_add((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v0 = v0.wrapping_add((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+            sum = sum.wrapping_add(DELTA);
+            v1 = v1.wrapping_add((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+        }
+
+        let v = block.get_out();
+        v[0..4].copy_from_slice(&v0.to_le_bytes());
+        v[4..8].copy_from_slice(&v1.to_le_bytes());
+    }
+    decrypt: {
+        let v = block.get_in();
+        let mut v0 = u32::from_le_bytes(v[0..4].try_into().unwrap());
+        let mut v1 = u32::from_le_bytes(v[4..8].try_into().unwrap());
+        let mut sum = DELTA.wrapping_mul(ROUNDS as u32);
+
+        // Same as encrypt, just in reverse
+        for _ in 0..8 {
+            v1 = v1.wrapping_sub((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+            sum = sum.wrapping_sub(DELTA);
+            v0 = v0.wrapping_sub((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v1 = v1.wrapping_sub((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+            sum = sum.wrapping_sub(DELTA);
+            v0 = v0.wrapping_sub((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v1 = v1.wrapping_sub((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+            sum = sum.wrapping_sub(DELTA);
+            v0 = v0.wrapping_sub((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+        }
+        for _ in 0..8 {
+            v1 = v1.wrapping_sub((((v0 << 4) ^ (v0 >> 5)).wrapping_add(v0)) ^ sum.wrapping_add(cipher.k[((sum >> 11) & 3) as usize]));
+            sum = sum.wrapping_sub(DELTA);
+            v0 = v0.wrapping_sub((((v1 << 4) ^ (v1 >> 5)).wrapping_add(v1)) ^ sum.wrapping_add(cipher.k[(sum & 3) as usize]));
+        }
+
+        let v = block.get_out();
+        v[0..4].copy_from_slice(&v0.to_le_bytes());
+        v[4..8].copy_from_slice(&v1.to_le_bytes());
+    }
+);
