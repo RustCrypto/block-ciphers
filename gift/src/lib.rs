e96cb0355e89eab462031c2cb4d94c863ae65459
schmid7k <54248317+Schmid7k@users.noreply.github.com>
2024-07-29 02:02:45 +0200

    Gift (#322)
    This pull request contains a constant-time software fixslicing
    implementation of the [GIFT](https://eprint.iacr.org/2017/622.pdf) block
    cipher, based on the C implementation of the original authors found
    [here](https://github.com/aadomn/gift/tree/master/crypto_bc/gift128/opt32).
    
    Gift is a PRESENT based block cipher with focus on energy efficiency and
    a small memory footprint making it ideal for usage in resource
    constrained environments. It gained some popularity as part of
    GIFT-COFB, which is a finalist in the current [NIST lightweight
    cryptography
    competition](https://csrc.nist.gov/Projects/lightweight-cryptography) as
    well as SUNDAE-GIFT.
    
    This pull request comes with an implementation of GIFT-128, meaning Gift
    operating on 128-bit blocks, though I am planning on implementing
    GIFT-64 in the future.
    
    It passes all test vectors and performs at ~44.5 cpb on an Intel Core i7
    8700k with 3.7GHz core clock, while the C version "only" performs at
    ~47.1 cpb on the same machine.

---
+++ gift/src/lib.rs
@@ -0,0 +1,53 @@
+//! Pure Rust implementation of the [Gift][1] block cipher.
+//!
+//! # ⚠️ Security Warning: Hazmat!
+//!
+//! This crate implements only the low-level block cipher function, and is intended
+//! for use for implementing higher-level constructions *only*. It is NOT
+//! intended for direct use in applications.
+//!
+//! USE AT YOUR OWN RISK!
+//!
+//! # Examples
+//! ```
+//! use gift_cipher::cipher::generic_array::GenericArray;
+//! use gift_cipher::cipher::{BlockDecrypt, BlockEncrypt, KeyInit};
+//! use gift_cipher::Gift128;
+//!
+//! let key = GenericArray::from([0u8; 16]);
+//! let mut block = GenericArray::from([0u8; 16]);
+//!
+//! // Initialize cipher
+//! let cipher = Gift128::new(&key);
+//!
+//! let block_copy = block;
+//!
+//! // Encrypt block in-place
+//! cipher.encrypt_block(&mut block);
+//!
+//! // And decrypt it back
+//! cipher.decrypt_block(&mut block);
+//!
+//! assert_eq!(block, block_copy);
+//! ```
+//!
+//! [1]: https://eprint.iacr.org/2017/622.pdf
+
+#![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
+    html_root_url = "https://docs.rs/gift-cipher/0.0.1"
+)]
+#![deny(unsafe_code)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![warn(missing_docs, rust_2018_idioms)]
+
+pub use cipher;
+
+mod consts;
+mod gift;
+mod key_schedule;
+mod primitives;
+
+pub use crate::gift::Gift128;
