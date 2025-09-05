bcedd162243f52d5dca34b7b9cc67a02114bb29b
Artyom Pavlov <newpavlov@gmail.com>
2025-09-05 17:56:56 +0300

    Migrate to new dev macro (#502)

--- idea/tests/mod.rs
+++ idea/tests/mod.rs
@@ -1,5 +1,4 @@
 //! Test vectors from:
 //! https://www.cosic.esat.kuleuven.be/nessie/testvectors/bc/idea/Idea-128-64.verified.test-vectors
 
-// TODO(tarcieri): update tests to support RustCrypto/traits#1916
-//cipher::block_cipher_test!(idea_test, "idea", idea::Idea);
+cipher::block_cipher_test!(idea, idea::Idea);
