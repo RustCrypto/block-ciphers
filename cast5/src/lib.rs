f4682300988b2965582f2b42618c8208fa13e0e5
Shun Sakai <sorairolake@protonmail.ch>
2026-03-05 07:38:19 +0900

    Remove unnecessary `clone`s from examples and tests (#540)

--- cast5/src/lib.rs
+++ cast5/src/lib.rs
@@ -18,7 +18,7 @@
 //! // Initialize cipher
 //! let cipher = Cast5::new(&key);
 //!
-//! let block_copy = block.clone();
+//! let block_copy = block;
 //! // Encrypt block in-place
 //! cipher.encrypt_block(&mut block);
 //! // And decrypt it back
